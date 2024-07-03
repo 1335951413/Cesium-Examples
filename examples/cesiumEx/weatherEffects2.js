439fd9fbf36b0f602cc2ef785858977aa739bd58
jiawanlong <trampjwl1@163.com>
2024-07-03 17:09:07 +0800

    Update weatherEffects2.js

--- examples/cesiumEx/weatherEffects2.js
+++ examples/cesiumEx/weatherEffects2.js
@@ -56,7 +56,51 @@
       return stage;
     }
     fs_snow(){
-      return ``;
+      return  `
+    #version 300 es
+    precision highp float;
+
+    uniform sampler2D colorTexture;
+    in vec2 v_textureCoordinates;
+    out vec4 fragColor;
+
+    float snow(vec2 uv, float scale) {
+      float time = float(czm_frameNumber) / 60.0;
+      float w = smoothstep(1.0, 0.0, -uv.y * (scale / 10.0));
+      if (w < 0.1) return 0.0;
+      uv += time / scale;
+      uv.y += time * 2.0 / scale;
+      uv.x += sin(uv.y + time * 0.5) / scale;
+      uv *= scale;
+      vec2 s = floor(uv), f = fract(uv), p;
+      float k = 3.0, d;
+      p = 0.5 + 0.35 * sin(11.0 * fract(sin((s + p + scale) * mat2(7, 3, 6, 5)) * 5.0)) - f;
+      d = length(p);
+      k = min(d, k);
+      k = smoothstep(0.0, k, sin(f.x + f.y) * 0.01);
+      return k * w;
+    }
+
+    void main() {
+      vec2 resolution = czm_viewport.zw;
+      vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
+      vec3 finalColor = vec3(0.0);
+      float c = 0.0;
+      c += snow(uv, 30.0) * 0.0;
+      c += snow(uv, 20.0) * 0.0;
+      c += snow(uv, 15.0) * 0.0;
+      c += snow(uv, 10.0);
+      c += snow(uv, 8.0);
+      c += snow(uv, 6.0);
+      c += snow(uv, 5.0);
+      finalColor = vec3(c);
+
+      // 调整亮度
+      finalColor = finalColor * 1.2; // 提升亮度，可以根据需要调整这个值
+
+      fragColor = mix(texture(colorTexture, v_textureCoordinates), vec4(finalColor, 1.0), 0.2);
+    }
+  `;
     }
     fs_rain(){
       return   `
@@ -92,17 +136,18 @@
     fs_fog(){
       return 'uniform sampler2D colorTexture;\n'
           +'  uniform sampler2D depthTexture;\n' 
-          +'  varying vec2 v_textureCoordinates;\n'
+          +'  in vec2 v_textureCoordinates;\n'
+          +'  out vec4 fragColor;\n'
           +'  void main(void)\n'
           +'  {\n'
-          +'      vec4 origcolor=texture2D(colorTexture, v_textureCoordinates);\n'
+          +'      vec4 origcolor=texture(colorTexture, v_textureCoordinates);\n'
           +'      vec4 fogcolor=vec4(0.8,0.8,0.8,0.5);\n'
           +'      float depth = czm_readDepth(depthTexture, v_textureCoordinates);\n'
-          +'      vec4 depthcolor=texture2D(depthTexture, v_textureCoordinates);\n'
+          +'      vec4 depthcolor=texture(depthTexture, v_textureCoordinates);\n'
           +'      float f=(depthcolor.r-0.22)/0.2;\n'
           +'      if(f<0.0) f=0.0;\n'
           +'      else if(f>1.0) f=1.0;\n'
-          +'      gl_FragColor = mix(origcolor,fogcolor,0.8);\n'
+          +'      fragColor = mix(origcolor,fogcolor,0.8);\n'
           +'   }';
     }
 }
