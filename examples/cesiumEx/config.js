c6fecea1b7dec574301290c669de1859c142e007
jiawanlong <trampjwl1@163.com>
2024-08-26 10:13:17 +0800

    Update config.js

--- examples/cesiumEx/config.js
+++ examples/cesiumEx/config.js
@@ -24,18 +24,15 @@ var exampleConfig = {
                         fileName: "0"
                     },
                     //  {
-                    
                     //     name: "台风",
                     //     name_en: "台风",
                     //     thumbnail: "案例2.png",
-
                     //     fileName: "1"
                     // },
                     {
                         name: "下雨+闪电+洪水",
                         name_en: "下雨+闪电+洪水",
                         thumbnail: "2.png",
-
                         fileName: "2"
                     },
 
