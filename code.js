b20244424d59caae2c6c6bf6ec5a90b8097c1528
jiawanlong <trampjwl1@163.com>
2025-01-03 09:58:53 +0800

    不弹窗授权码

---
+++ code.js
@@ -0,0 +1,14 @@
+
+let authorizeCode = [
+    {
+        'id': 'aa1ce9548a4110da75b1276f70d6bb7d',
+        'endTime': '2500-12-31 24:00:00',
+        'name': 'jiawanlong',
+    },
+    {
+        'id': '9f989d38fe9200769b0acdec6b7fcfa7',
+        'endTime': '2025-01-03 24:00:00',
+        'name': 'wx-wh-lucky',
+    }
+]
+
