74e440f96b45f5e4fc1b5614fdd2ecc750055eda
jiawanlong <trampjwl1@163.com>
2025-01-10 11:21:54 +0800

    Update code.js

--- code.js
+++ code.js
@@ -6,6 +6,11 @@ let authorizeCode = [
         'name': 'jiawanlong',
     },
     {
+        'id': 'd411156db30937b6a865d95f0145154b',
+        'endTime': '2500-12-31 24:00:00',
+        'name': 'glj',
+    },
+    {
         'id': '9f989d38fe9200769b0acdec6b7fcfa7',
         'endTime': '2025-01-03 24:00:00',
         'name': 'wx-wh-lucky',
